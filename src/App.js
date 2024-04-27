057cbc673ea88e48c53207de957252f44516d6af
Amrutha-01 <amrutharokkam573@gmail.com>
2024-04-28 01:15:41 +0530

    features-comp

--- src/App.js
+++ src/App.js
@@ -9,10 +9,13 @@ import GiveRide from "./components/GiveRide/GiveRide";
 import LEWorkGet from "./components/LEWorkGet/LEWorkGet";
 import MobileScroll from "./components/Features/Featuresshow";
 import money from "./components/images/Designer (2).png";
+import img1 from "./components/images/Designer (2) (7).svg";
+import img2 from "./components/images/Saving money-amico.svg";
+import img3 from "./components/images/location.svg";
 
 function App() {
   const getRideData = {
-    title: "How LifeEase Works?",
+    title: "How LiftEase Works?",
     description:
       "Tempor reprehenderit nostrud anim aute pariatur elit cillum excepteur ad culpa nisi. Eu amet anim deserunt veniam irure excepteur laborum in. Reprehenderit consequat adipisicing eu nostrud et pariatur eu id et nisi voluptate. Voluptate minim ullamco nostrud adipisicing sit officia cupidatat do non labore dolore adipisicing cupidatat. Pariatur labore sunt laboris anim laborum non.",
     cardsData: [
@@ -34,7 +37,7 @@ function App() {
     ],
   };
   const giveRideData = {
-    title: "How LifeEase Works?",
+    title: "How LiftEase Works?",
     description:
       "Tempor reprehenderit nostrud anim aute pariatur elit cillum excepteur ad culpa nisi. Eu amet anim deserunt veniam irure excepteur laborum in. Reprehenderit consequat adipisicing eu nostrud et pariatur eu id et nisi voluptate. Voluptate minim ullamco nostrud adipisicing sit officia cupidatat do non labore dolore adipisicing cupidatat. Pariatur labore sunt laboris anim laborum non.",
     cardsData: [
@@ -55,6 +58,46 @@ function App() {
       },
     ],
   };
+  const getRideScroll = [
+    {
+      heading: "Track your each Rupee with Bills",
+      description:
+        "Securely Pool Bills for Smooth Transactions and Strengthen Friendships",
+      lappy_img: img1,
+    },
+    {
+      heading: "Enhance Financial Bonds",
+      description:
+        "Get your money back with the periodic interest by enabling Financial bonds",
+      lappy_img: img2,
+    },
+    {
+      heading: "Split Expenses Smoothly",
+      description:
+        "Effortlessly divide and manage expenses among friends while maintaining transparency and trust. Bills for Smooth Transactions and Strengthen Friendships",
+      lappy_img: img3,
+    },
+  ];
+  const giveRideScroll = [
+    {
+      heading: "Track your each Rupee with Bills",
+      description:
+        "Securely Pool Bills for Smooth Transactions and Strengthen Friendships",
+      lappy_img: img1,
+    },
+    {
+      heading: "Enhance Financial Bonds",
+      description:
+        "Get your money back with the periodic interest by enabling Financial bonds",
+      lappy_img: img2,
+    },
+    {
+      heading: "Split Expenses Smoothly",
+      description:
+        "Effortlessly divide and manage expenses among friends while maintaining transparency and trust. Bills for Smooth Transactions and Strengthen Friendships",
+      lappy_img: img3,
+    },
+  ];
   return (
     <div className="App">
       <Router>
@@ -66,7 +109,7 @@ function App() {
               <div>
                 <Navbar />
                 <GetRide />
-                <MobileScroll />
+                <MobileScroll scrollData={getRideData}/>
                 <LEWorkGet data={getRideData} />
                 <Footer />
               </div>
@@ -78,7 +121,7 @@ function App() {
               <div>
                 <Navbar />
                 <GiveRide />
-                <MobileScroll />
+                <MobileScroll scrollData={giveRideScroll}/>
                 <LEWorkGet data={giveRideData} />
                 <Footer />
               </div>
--- src/components/Features/Featuresshow.js
+++ src/components/Features/Featuresshow.js
@@ -1,31 +1,9 @@
 import { React, useState } from "react";
 import "./Featureshow.css";
 import ScreenText from "./ScreenText.js";
-import img1 from "../images/Designer (2) (7).svg";
-import img2 from "../images/Saving money-amico.svg";
-import img3 from "../images/location.svg";
 
-const scrollData = [
-  {
-    heading: "Track your each Rupee with Bills",
-    description:
-      "Securely Pool Bills for Smooth Transactions and Strengthen Friendships",
-    lappy_img: img1,
-  },
-  {
-    heading: "Enhance Financial Bonds",
-    description:
-      "Get your money back with the periodic interest by enabling Financial bonds",
-    lappy_img: img2,
-  },
-  {
-    heading: "Split Expenses Smoothly",
-    description:
-      "Effortlessly divide and manage expenses among friends while maintaining transparency and trust. Bills for Smooth Transactions and Strengthen Friendships",
-    lappy_img: img1,
-  },
-];
-const MobileScroll = () => {
+
+const MobileScroll = ({scrollData}) => {
   const [currentImg, setCurrentImg] = useState(0);
   return (
     <div className="max-width flex mobile-scroll">
